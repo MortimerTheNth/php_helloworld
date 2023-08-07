e2b56fe0e49fb0ac9ebae130c059c59593dcbf52
Onur <onatsurmeli@gmail.com>
2023-08-07 04:09:08 +0200

    added sections and basic partitions for the layout

--- index.php
+++ index.php
@@ -10,6 +10,155 @@
         <title>Hello World but it's responsive!</title>
     </head>
     <body>
-        <h1></h1>
+        <header>
+            <ul>
+                <li><a href="#Home">Home</a></li>
+                <li><a href="#Movies">Movies</a></li>
+                <li><a href="#Shows">TV Shows</a></li>
+                <li><a href="#Top100">Top 100</a></li>
+                <li><a href="#About">About</a></li>
+            </ul>
+        </header>
+        <h1>Bob's Movie and Show Emproium</h1>
+        <img src="" alt="hero image">
+        <h2><i>Best movies and shows, by someone not named Bob.</i></h2>
+        <main>
+            <section class="featured">
+                <h3>Featured This Week</h3>
+                <div class="featured-item">
+                    <img src="" alt="featured movie">
+                    <h4>Featured Movie</h4>
+                    <p>Movie Title</p>
+                </div>
+                <div class="featured-item">
+                    <img src="" alt="featured show">
+                    <h4>Featured Show</h4>
+                    <p>Show Title</p>
+                </div>
+            </section>
+            <section class="movies-container">
+                <h3>Movies</h3>
+                <div class="row">
+                    <div class="movie-column-item">
+                        <img src="" alt="">
+                        <div class="movie-title"></div>
+                        <p></p>
+                    </div>
+                    <div class="movie-column-item">
+                        <img src="" alt="">
+                        <div class="movie-title"></div>
+                        <p></p>
+                    </div>
+                    <div class="movie-column-item">
+                        <img src="" alt="">
+                        <div class="movie-title"></div>
+                        <p></p>
+                    </div>
+                    <div class="movie-column-item">
+                        <img src="" alt="">
+                        <div class="movie-title"></div>
+                        <p></p>
+                    </div>
+                    <div class="movie-column-item">
+                        <img src="" alt="">
+                        <div class="movie-title"></div>
+                        <p></p>
+                    </div>
+                    <div class="movie-column-item">
+                        <img src="" alt="">
+                        <div class="movie-title"></div>
+                        <p></p>
+                    </div>
+                    <div class="movie-column-item">
+                        <img src="" alt="">
+                        <div class="movie-title"></div>
+                        <p></p>
+                    </div>
+                    <div class="movie-column-item">
+                        <img src="" alt="">
+                        <div class="movie-title"></div>
+                        <p></p>
+                    </div>
+                    <div class="movie-column-item">
+                        <img src="" alt="">
+                        <div class="movie-title"></div>
+                        <p></p>
+                    </div>
+                    <div class="movie-column-item">
+                        <img src="" alt="">
+                        <div class="movie-title"></div>
+                        <p></p>
+                    </div>
+                </div>
+            </section>
+            <section class="shows-container">
+                <h3>TV Shows</h3>
+                <div class="row">
+                    <div class="show-column-item">
+                        <img src="" alt="">
+                        <div class="show-title"></div>
+                        <p>Lorem ipsum dolor sit.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, dicta.</p>
+                    </div>
+                    <div class="show-column-item">
+                        <img src="" alt="">
+                        <div class="show-title"></div>
+                        <p>Hic expedita assumenda similique?</p>
+                        <p>Deserunt blanditiis ipsum magni ducimus cumque earum illo assumenda exercitationem.</p>
+                    </div>
+                    <div class="show-column-item">
+                        <img src="" alt="">
+                        <div class="show-title"></div>
+                        <p>Id quas sapiente deserunt?</p>
+                        <p>Et laboriosam delectus hic? Nam accusantium minus nobis consectetur impedit.</p>
+                    </div>
+                    <div class="show-column-item">
+                        <img src="" alt="">
+                        <div class="show-title"></div>
+                        <p>Libero itaque deserunt numquam!</p>
+                        <p>Sapiente recusandae vitae neque fugit tenetur commodi est, voluptatem molestias.</p>
+                    </div>
+                    <div class="show-column-item">
+                        <img src="" alt="">
+                        <div class="show-title"></div>
+                        <p>Nesciunt recusandae quae accusamus.</p>
+                        <p>Earum itaque sint facilis reprehenderit provident pariatur obcaecati error veritatis.</p>
+                    </div>
+                    <div class="show-column-item">
+                        <img src="" alt="">
+                        <div class="show-title"></div>
+                        <p>Veritatis cum quisquam dolor!</p>
+                        <p>Quisquam possimus laudantium magni. Quisquam numquam facilis reiciendis et eveniet!</p>
+                    </div>
+                    <div class="show-column-item">
+                        <img src="" alt="">
+                        <div class="show-title"></div>
+                        <p>Ullam quos consequuntur unde.</p>
+                        <p>Perferendis expedita nostrum consequatur eligendi numquam in officia nam sapiente.</p>
+                    </div>
+                    <div class="show-column-item">
+                        <img src="" alt="">
+                        <div class="show-title"></div>
+                        <p>Eos, autem maiores. Quaerat.</p>
+                        <p>Nesciunt aperiam perferendis assumenda animi veniam reiciendis dolorum quis quasi!</p>
+                    </div>
+                    <div class="show-column-item">
+                        <img src="" alt="">
+                        <div class="show-title"></div>
+                        <p>Quam odio sequi velit?</p>
+                        <p>Veritatis iste exercitationem ab tempora error eaque sunt asperiores porro?</p>
+                    </div>
+                    <div class="show-column-item">
+                        <img src="" alt="">
+                        <div class="show-title"></div>
+                        <p>Earum in provident officia.</p>
+                        <p>Molestias omnis ipsam, aspernatur nobis nulla sint? Libero, harum debitis!</p>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer>
+            <p>&copy; 2019 Bob's Movie and Show Emporium</p>
+        </footer>
     </body>
 </html>
