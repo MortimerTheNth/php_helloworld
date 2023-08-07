99193fab6521683b6a08d0dee1eb9a5fa6a241c3
Onur <105538960+MortimerTheNth@users.noreply.github.com>
2023-08-07 03:14:50 +0200

    add meta tags into the project head

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=devide-width, initial-scale=1.0">
-        <title>Hello World!</title>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="author" content="Onur Surmeli">
+        <meta name="description" content="Hello World!">
+        <meta name="keywords" content="Hello World, PHP, responsive, dynamic, practice, project">
+        <title>Hello World but it's responsive!</title>
     </head>
     <body>
-        <h1><?php echo 'Hello World!'; ?></h1>
+        <h1></h1>
     </body>
 </html>
