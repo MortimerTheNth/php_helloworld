23b95348a97526a911345059162e1ae4933e932c
Onur Surmeli <onatsurmeli@gmail.com>
2023-08-15 19:51:39 +0200

    adjust nesting in sass, rename row item class, add responsive styling

--- index.php
+++ index.php
@@ -40,52 +40,52 @@
             <section class="titles-container">
                 <h3>Movies</h3>
                 <div class="row">
-                    <div class="movie-column-item">
+                    <div class="column-item">
                         <div class="movie-title"></div>
                         <p>Lorem ipsum dolor sit.</p>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, sit.</p>
                     </div>
-                    <div class="movie-column-item">
+                    <div class="column-item">
                         <div class="movie-title"></div>
                         <p>Amet cumque quasi porro?</p>
                         <p>Tempora repudiandae asperiores molestias, sint minima vero doloremque expedita nemo.</p>
                     </div>
-                    <div class="movie-column-item">
+                    <div class="column-item">
                         <div class="movie-title"></div>
                         <p>Neque reiciendis ea molestiae.</p>
                         <p>Tenetur a voluptates dignissimos ex aperiam iusto, unde praesentium sapiente?</p>
                     </div>
-                    <div class="movie-column-item">
+                    <div class="column-item">
                         <div class="movie-title"></div>
                         <p>Dolor delectus nam enim.</p>
                         <p>Cum velit recusandae in. Deserunt nihil culpa velit. Molestias, recusandae.</p>
                     </div>
-                    <div class="movie-column-item">
+                    <div class="column-item">
                         <div class="movie-title"></div>
                         <p>Deleniti dolor aliquid magni.</p>
                         <p>Odio ex maiores et amet mollitia, iusto sit esse repellendus.</p>
                     </div>
-                    <div class="movie-column-item">
+                    <div class="column-item">
                         <div class="movie-title"></div>
                         <p>At error minus atque.</p>
                         <p>Voluptate quidem odio ipsa qui quibusdam illum placeat provident fugiat?</p>
                     </div>
-                    <div class="movie-column-item">
+                    <div class="column-item">
                         <div class="movie-title"></div>
                         <p>Soluta voluptate ducimus debitis.</p>
                         <p>Quaerat molestiae odit voluptatibus in aut ex modi, amet enim!</p>
                     </div>
-                    <div class="movie-column-item">
+                    <div class="column-item">
                         <div class="movie-title"></div>
                         <p>Nobis officiis vel deserunt.</p>
                         <p>Vel ad similique quos sapiente laudantium ab maxime velit laboriosam?</p>
                     </div>
-                    <div class="movie-column-item">
+                    <div class="column-item">
                         <div class="movie-title"></div>
                         <p>Adipisci natus atque quia.</p>
                         <p>Eos debitis eligendi beatae illo adipisci incidunt, sapiente vero odio.</p>
                     </div>
-                    <div class="movie-column-item">
+                    <div class="column-item">
                         <div class="movie-title"></div>
                         <p>Consequatur, sapiente officiis! Iste?</p>
                         <p>Excepturi porro neque quisquam cum enim magnam doloremque ex quia!</p>
@@ -95,54 +95,54 @@
             <section class="titles-container">
                 <h3>TV Shows</h3>
                 <div class="row">
-                    <div class="show-column-item">
+                    <div class="column-item">
                         <div class="show-title"></div>
                         <p>Lorem ipsum dolor sit.</p>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, dicta.</p>
                     </div>
-                    <div class="show-column-item">
+                    <div class="column-item">
                         <div class="show-title"></div>
                         <p>Hic expedita assumenda similique?</p>
                         <p>Deserunt blanditiis ipsum magni ducimus cumque earum illo assumenda exercitationem.</p>
                     </div>
-                    <div class="show-column-item">
+                    <div class="column-item">
                         <div class="show-title"></div>
                         <p>Id quas sapiente deserunt?</p>
                         <p>Et laboriosam delectus hic? Nam accusantium minus nobis consectetur impedit.</p>
                     </div>
-                    <div class="show-column-item">
+                    <div class="column-item">
                         <div class="show-title"></div>
                         <p>Libero itaque deserunt numquam!</p>
                         <p>Sapiente recusandae vitae neque fugit tenetur commodi est, voluptatem molestias.</p>
                     </div>
-                    <div class="show-column-item">
+                    <div class="column-item">
                         <div class="show-title"></div>
                         <p>Nesciunt recusandae quae accusamus.</p>
                         <p>Earum itaque sint facilis reprehenderit provident pariatur obcaecati error veritatis.</p>
                     </div>
-                    <div class="show-column-item">
+                    <div class="column-item">
                         <div class="show-title"></div>
                         <p>Veritatis cum quisquam dolor!</p>
                         <p>Quisquam possimus laudantium magni. Quisquam numquam facilis reiciendis et eveniet!</p>
                     </div>
-                    <div class="show-column-item">
+                    <div class="column-item">
                         
                         <div class="show-title"></div>
                         <p>Ullam quos consequuntur unde.</p>
                         <p>Perferendis expedita nostrum consequatur eligendi numquam in officia nam sapiente.</p>
                     </div>
-                    <div class="show-column-item">
+                    <div class="column-item">
                         
                         <div class="show-title"></div>
                         <p>Eos, autem maiores. Quaerat.</p>
                         <p>Nesciunt aperiam perferendis assumenda animi veniam reiciendis dolorum quis quasi!</p>
                     </div>
-                    <div class="show-column-item">
+                    <div class="column-item">
                         <div class="show-title"></div>
                         <p>Quam odio sequi velit?</p>
                         <p>Veritatis iste exercitationem ab tempora error eaque sunt asperiores porro?</p>
                     </div>
-                    <div class="show-column-item">
+                    <div class="column-item">
                         <div class="show-title"></div>
                         <p>Earum in provident officia.</p>
                         <p>Molestias omnis ipsam, aspernatur nobis nulla sint? Libero, harum debitis!</p>
